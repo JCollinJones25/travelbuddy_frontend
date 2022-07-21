ebcacf66319af39b1803d7fbe409e6f178c7fff9
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-07-21 13:47:27 -0400

    new row in table is created when form is submitted but fields are null

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -12,52 +12,56 @@ const [trip, setTrip] = useState({
     reservations: ""
 })
 
-const URL = "http://localhost:4000/trips"
+const URL = "http://localhost:4000/trips/new"
 
 const handleSubmit = async (e) => {
-    fetch(URL)
-    .then(response => response.json())
-    .then(result => {
-      console.log(result)
-    })
-    // e.preventDefault()   
-    //     try {
-    //     const body = { location, hotel, flights, days, nights, activities, reservations }
-    //     const response = await fetch(URL, {
-    //         "method": "POST",
-    //         "headers": { "Content-Type": "application/json" },
-    //         "body": JSON.stringify(body)
-    //     })
-    //     console.log(response)
-    // } catch (err) {
-    //     console.error(err.message)
-    // }
+    e.preventDefault()   
+    // fetch(URL)
+    // .then(response => response.json())
+    // .then(result => {
+    //   console.log(result)
+    // })
+        try {
+        const body = { trip }
+        const response = await fetch(URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body)
+        })
+        console.log(response)
+    } catch (err) {
+        console.error(err.message)
+    }
 }
 
-const handleChange = () => {
-
+const handleChange = (e) => {
+    e.preventDefault()
+    const value = e.target.value
+    setTrip({
+        [e.target.name]: value
+    })
 }
 
     return (
        
         <div className="text-center mt-5">
-            <h3>Add a new upcoming trip</h3>
+            <h3>Add an upcoming trip</h3>
             <form className="m-5" onSubmit={handleSubmit}>
                 <p>City, State</p>
-                <input type="text" className="form-control" value={trip.location} onChange={handleChange}/>
+                <input type="text" className="form-control mb-3" value={trip.location} onChange={handleChange}/>
                 <p>Hotel(s)</p>
-                <input type="text" className="form-control" value={trip.hotel} onChange={handleChange}/>
+                <input type="text" className="form-control mb-3" value={trip.hotel} onChange={handleChange}/>
                 <p>Flight(s)</p>
-                <input type="text" className="form-control" value={trip.flights} onChange={handleChange}/>
+                <input type="text" className="form-control mb-3" value={trip.flights} onChange={handleChange}/>
                 <p>Days</p>
-                <input type="text" className="form-control" value={trip.days} onChange={handleChange}/>
+                <input type="text" className="form-control mb-3" value={trip.days} onChange={handleChange}/>
                 <p>Nights</p>
-                <input type="text" className="form-control" value={trip.nights} onChange={handleChange}/>
+                <input type="text" className="form-control mb-3" value={trip.nights} onChange={handleChange}/>
                 <p>Activities</p>
-                <input type="text" className="form-control" value={trip.activities} onChange={handleChange}/>
+                <input type="text" className="form-control mb-3" value={trip.activities} onChange={handleChange}/>
                 <p>Reservations</p>
-                <input type="text" className="form-control" value={trip.reservations} onChange={handleChange}/>
-                <button className="btn btn-sucess m-3">Add</button>
+                <input type="text" className="form-control mb-3" value={trip.reservations} onChange={handleChange}/>
+                <button className="btn btn-sucess">Add</button>
                 </form>
         </div>
     )
