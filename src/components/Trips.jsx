99fa8a3e5d0c65a153025bb6e63f3e403b4b7ed4
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-07-26 14:28:24 -0400

    data is rendering

--- src/components/Trips.jsx
+++ src/components/Trips.jsx
@@ -2,33 +2,44 @@ import { useState, useEffect } from 'react'
 
 const Trips = (props) => {
 
-    const [data, setData] = useState(null)
+    const [trips, setTrips] = useState([])
 
     const URL = "http://localhost:4000/trips"
 
-    const getData = async () => {
-        const response = await fetch(URL)
-        const Data = await response.json()
-        setData(Data)
+    const getTrips = async () => {
+        try {
+            const response = await fetch(URL)
+            const result = await response.json()
+            setTrips(result)
+        } catch (err) {
+            console.error(err.message)
+        }
     }
 
     useEffect(() => {
-        getData()
+        getTrips()
     }, [])
 
+    console.log(trips)
 
     return (
         <div className="trips">
         <h3>Trips</h3>
         {" "}
         <table className="table text-center">
-            <thread>
-                <tr>
-                    <th>Trip</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                </tr>
-            </thread>
+            <tbody>
+                {trips.map(trip => (
+                    <tr>
+                        <td>{trip.location}</td>
+                        <td>{trip.hotel}</td>
+                        <td>{trip.flights}</td>
+                        <td>{trip.days}</td>
+                        <td>{trip.nights}</td>
+                        <td>Edit</td>
+                        <td>Delete</td>
+                    </tr>
+                ))}
+            </tbody>
         </table>
         </div>
     )
