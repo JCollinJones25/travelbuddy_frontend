243e5df255ad455da62f7a2c1b850f2311d00071
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-07-27 14:43:39 -0400

    class changed to className

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -19,17 +19,17 @@ const Edit = ({trip}) => {
       
       <button
         type="button"
-        class="btn btn-primary"
+        className="btn btn-primary"
         data-toggle="modal"
         data-target={`#id${trip.id}`}>Edit</button>
 
-      <div class="modal" id={`id${trip.id}`}>
-        <div class="modal-dialog">
-          <div class="modal-content">
+      <div className="modal" id={`id${trip.id}`}>
+        <div className="modal-dialog">
+          <div className="modal-content">
            
-            <div class="modal-header">
-              <h4 class="modal-title">Edit {location}</h4>
-              <button type="button" class="close" data-dismiss="modal">
+            <div className="modal-header">
+              <h4 className="modal-title">Edit {location}</h4>
+              <button type="button" className="close" data-dismiss="modal">
                 &times;
               </button>
             </div>
@@ -45,10 +45,10 @@ const Edit = ({trip}) => {
             </div>
 
             <div class="modal-footer">
-              <button type="button" class="btn btn-warning" data-dismiss="modal">
+              <button type="button" className="btn btn-warning" data-dismiss="modal">
                 Edit
               </button>
-              <button type="button" class="btn btn-danger" data-dismiss="modal">
+              <button type="button" className="btn btn-danger" data-dismiss="modal">
                 Close
               </button>
             </div>
