577a5a5dc78f4b28961466b9d33a1806042dd0d2
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-07-29 17:13:53 -0400

    design responsive

--- src/components/Trips.jsx
+++ src/components/Trips.jsx
@@ -1,72 +1,79 @@
-import { useState, useEffect } from 'react'
-import Edit from "./Edit"
+import { useState, useEffect } from "react";
+import Edit from "./Edit";
 
 const Trips = () => {
+  const [trips, setTrips] = useState([]);
 
-    const [trips, setTrips] = useState([])
+  const URL = "http://localhost:4000/trips";
 
-    const URL = "http://localhost:4000/trips"
-
-    const getTrips = async () => {
-        try {
-            const response = await fetch(URL)
-            const result = await response.json()
-            setTrips(result)
-        } catch (err) {
-            console.error(err.message)
-        }
+  const getTrips = async () => {
+    try {
+      const response = await fetch(URL);
+      const result = await response.json();
+      setTrips(result);
+    } catch (err) {
+      console.error(err.message);
     }
+  };
 
-    useEffect(() => {
-        getTrips()
-    }, [])
+  useEffect(() => {
+    getTrips();
+  }, []);
 
-    const deleteTrip = async (id) => {
-        try {
-            const deleteTrip = await fetch(`${URL}/${id}`, {
-                method: "DELETE"
-            })
-            setTrips(trips.filter(trip => trip.id !== id))
-        } catch (err) {
-            console.error(err.message)
-        }
+  const deleteTrip = async (id) => {
+    try {
+      const deleteTrip = await fetch(`${URL}/${id}`, {
+        method: "DELETE",
+      });
+      setTrips(trips.filter((trip) => trip.id !== id));
+    } catch (err) {
+      console.error(err.message);
     }
+  };
 
-    return (
-        <div className="trips">
-        <h5>Upcoming Trips</h5>
-        <hr></hr>
-        {" "}
+  return (
+    <div className="trips">
+      <h5>Upcoming Trips</h5>
+      <hr></hr>{" "}
         <table className="table">
-            <tr>
-                <th>Where?</th>
-                <th>Hotel</th>
-                <th>Flights</th>
-                <th>Days</th>
-                <th>Nights</th>
-                <th>Plans</th>
-                <th>Reservations</th>
-            </tr>
-            <tbody>
-                {trips.map(trip => (
-                    <tr key={trip.id}>
-                        <td>{trip.location}</td>
-                        <td>{trip.hotel}</td>
-                        <td>{trip.flights}</td>
-                        <td>{trip.days}</td>
-                        <td>{trip.nights}</td>
-                        <td>{trip.activities}</td>
-                        <td>{trip.reservations}</td>
-                        <td><Edit trip={trip} URL={URL}/></td>
-                        <td>
-                            <button className="btn btn-danger" onClick={() => deleteTrip(trip.id)}>Delete</button>
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
+          <tr>
+            <th>Where?</th>
+            <th>Hotel</th>
+            <th>Flights</th>
+            <th>Days</th>
+            <th>Nights</th>
+            <th>Plans</th>
+            <th>Reservations</th>
+          </tr>
+          <tbody>
+            {trips.map((trip) => (
+                <tr key={trip.id}>
+                <td>{trip.location}</td>
+                <td>{trip.hotel}</td>
+                <td>{trip.flights}</td>
+                <td>{trip.days}</td>
+                <td>{trip.nights}</td>
+                <td>{trip.activities}</td>
+                <td>{trip.reservations}</td>
+                <div className="edit-delete-buttons">
+                  <td>
+                    <Edit trip={trip} URL={URL} />
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => deleteTrip(trip.id)}
+                      >
+                      Delete
+                    </button>
+                  </td>
+                </div>
+              </tr>
+            ))}
+          </tbody>
         </table>
-        </div>
-    )
-}
+      </div>
+  );
+};
 
-export default Trips
+export default Trips;
